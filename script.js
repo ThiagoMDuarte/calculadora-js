858228e6f449ce7578b8a22b3d99197b2ea2cc9e
Thiago Duarte <104696554+ThiagoMDuarte@users.noreply.github.com>
2024-09-19 18:32:46 -0300

    Create function operate and add html buttons

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+let firstNumber = ''
+let secondNumber = ''
+let operator = ''
+let result = ''
+
+function operate (operator,firstNumber,secondNumber) {
+    if (operator === '+') {
+        add(firstNumber,secondNumber)
+    } else if (operator === '-') {
+        subtract(firstNumber,secondNumber)
+    }else if (operator === '*') {
+        multiply(firstNumber,secondNumber)
+    }else if (operator === '/') {
+        divide(firstNumber,secondNumber)
+    }
+}
+
 function add (num1, num2) {
     return num1 + num2
 }
